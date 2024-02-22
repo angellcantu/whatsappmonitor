770fd57deea86e79e57504a9e6bfc513a08f7dd6
DelosreyesBueno <carlosdelosreyes.efren@gmail.com>
2024-02-22 13:57:27 -0600

    New changes in cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://whatswatch.tamaulipas.app/',
-            "http://localhost:54476/"] 
+            "http://localhost:54476/"],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   });
+
   await app.listen(3000);
 }
 bootstrap();
